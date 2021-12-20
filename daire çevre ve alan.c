3f665a5cafb28b087e856f23ba645570ba96dfea
duhasamasti <96238861+duhasamasti@users.noreply.github.com>
2021-12-20 15:19:10 +0300

    Add files via upload

---
+++ daire çevre ve alan.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+#define pi 3.14
+
+int main(int argc, char *argv[]) {
+	float r,circumference,area;
+	
+	printf("Enter your radius");
+	scanf("%f",&r);
+	
+	circumference=2*pi*r;
+	area=pi*r^2;
+	
+	printf("circumference==> %f n\ area==> %f",circumference,area);
+	 
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
